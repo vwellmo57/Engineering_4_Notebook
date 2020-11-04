c82fcfadd39c4e6562c195e77fc04f615c0f5874
vwellmo57 <vwellmo57@charlottevilleschools.org>
2020-11-04 18:18:22 +0000

    added calc file

---
+++ Python/calculator.py
@@ -0,0 +1,13 @@
+def doMath():
+    print("pee")
+
+
+a = input("Enter the first number: ")
+b = input("Enter the second number: ") 
+
+
+print("Sum:\t\t" + doMath(a,b,1))
+print("Difference:\t" + doMath(a,b,2))
+print("Product:\t" + doMath(a,b,3))
+print("Quotient:\t" + doMath(a,b,4))
+print("Modulo:\t\t" + doMath(a,b,5))
