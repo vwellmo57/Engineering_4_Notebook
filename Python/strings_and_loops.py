40be39266bbf7eff64995b53ae14cf1ae8924cc5
vwellmo57 <55098291+vwellmo57@users.noreply.github.com>
2020-11-22 13:17:49 -0500

    Update strings_and_loops.py

--- Python/strings_and_loops.py
+++ Python/strings_and_loops.py
@@ -1,5 +1,12 @@
 words = 'This is random text we’re going to split apart'
-words2 = words.split(' ')
-print(type(words))
-for x in range(0, len(words2)):
-    print(words2[x])
+words = words.split(' ')
+y=0
+for x in range(0, len(words)):
+    #print(words[x][0])
+    for y in range(0,len(words[y])):
+        print(words[x][y])
+    
+    
+    print("-")
+    
+    
