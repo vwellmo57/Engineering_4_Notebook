613d1f7f6366faa08755a2ebe9454cc69dcbdd7c
vwellmo57 <55098291+vwellmo57@users.noreply.github.com>
2020-11-22 20:59:54 -0500

    Update strings_and_loops.py

--- Python/strings_and_loops.py
+++ Python/strings_and_loops.py
@@ -1,5 +1,4 @@
-words = input("Enter the sentence: ")
-words = words.split(' ')
+words = input("Enter the sentence: ").split(' ')
 for x in range(0, len(words)):
     for y in range(0,len(words[x])):
         print(words[x][y])
