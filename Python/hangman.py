bc1e89bbf4882714fc6a811cb4f3580c3f44945a
vwellmo57 <55098291+vwellmo57@users.noreply.github.com>
2020-12-07 12:59:03 -0500

    Update hangman.py

--- Python/hangman.py
+++ Python/hangman.py
@@ -1,17 +1,17 @@
-import random
-words = ["kitten", "dog", "mouse", "jeff", "puppy"]
-answerWord = words[random.randint(0,len(words)-1)]
-answerList = []
-lives = 5
-letterInput = " "
-rightGuess = 0
-winLose = None
-hangmanState=0
-uniqueChar=len(set(answerWord))
-for x in range(len(answerWord)):
+import random   #lets us generate a random number
+words = ["kitten", "dog", "mouse", "jeff", "puppy"]   #list of words
+answerWord = words[random.randint(0,len(words)-1)]  #picks a random word from list
+answerList = []   #making a list for the user answers
+lives = 5   #amount of lives
+letterInput = " " 
+rightGuess = 0  #amount of correct guesses
+winLose = None  #set to true or false, triggers an end game messege 
+hangmanState=0   #state of the hangman drawing
+uniqueChar=len(set(answerWord))   #finds the amount of unique characters
+for x in range(len(answerWord)):  #makes the answer array as long as the answer
     answerList.extend('_') 
 
-def hangman(hangmanState):
+def hangman(hangmanState):   #this is the array of the hangman drawings
     if hangmanState==0:
         print("---┐")
     if hangmanState==1:
@@ -40,29 +40,29 @@ def hangman(hangmanState):
         print("   |")
         print("  | |")
 
-#print(answerWord)
+#print(answerWord)  #prints the answer for testing
 print("Welcome to Hangman!")
-while lives>0:
-    letterInput = input("Enter a guess: ")
-    if letterInput in answerWord:
+while lives>0:  #while the user is alive
+    letterInput = input("Enter a guess: ")    #takes user input
+    if letterInput in answerWord:   #if they guessed a letter in the answer
         print("Correct!")
-        for x in range(len(answerWord)):
-            if letterInput == answerWord[x]:
-                answerList[x] = letterInput
-        print(answerList)
-        rightGuess = rightGuess + 1
-        hangman(hangmanState)
+        for x in range(len(answerWord)):  #runs the amount of characters in the answer
+            if letterInput == answerWord[x]: #if they match a letter
+                answerList[x] = letterInput   #sets it into the answer list
+        print(answerList)   #prints their progress
+        rightGuess = rightGuess + 1   #specifiying the state of the hangman drawing
+        hangman(hangmanState)   #calls the drawing function
         
-        if rightGuess == uniqueChar:
-            winLose = True
-            break
+        if rightGuess == uniqueChar:   #if they finish the word
+            winLose = True   
+            break       #exits the loop
     
     else:   
-        lives = lives-1
-        print("Wrong!")
-        hangman(hangmanState)
-        hangmanState = hangmanState + 1
-if winLose == True:
+        lives = lives-1   #lose a life
+        print("Wrong!")   #tell them
+        hangman(hangmanState)  #print the hangman
+        hangmanState = hangmanState + 1   #add one so that the drawing gets bigger next time
+if winLose == True:   #if they win tell them
     print("You Won!")
-else:
+else:    #if they lose tell them
     print("You Lost!")
