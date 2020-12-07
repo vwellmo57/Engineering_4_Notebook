8af7589fa9a74f323199f17549617b2cf0878c32
vwellmo57 <55098291+vwellmo57@users.noreply.github.com>
2020-12-07 13:05:31 -0500

    Update strings_and_loops.py

--- Python/strings_and_loops.py
+++ Python/strings_and_loops.py
@@ -1,1 +1,5 @@
-nice try philip
+words = input("Enter the sentence: ").split(' ')   #takes the input from the user and puts it in a list with a space in between
+for x in range(0, len(words)):  #a loop that runs for the length of the string
+    for y in range(0,len(words[x])):  #loop that runs for the length 
+        print(words[x][y])  #prints a letter from a part of the loop
+    print("-")  #prints - in between words
