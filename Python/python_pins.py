5fd5924b30bff31dbbce998235be5ef2d73f9846
vwellmo57 <55098291+vwellmo57@users.noreply.github.com>
2020-12-28 13:17:46 -0500

    Update python_pins.py

--- Python/python_pins.py
+++ Python/python_pins.py
@@ -1,9 +1,9 @@
-import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
-from time import sleep # Import the sleep function from the time module
-GPIO.setwarnings(False) # Ignore warning for now
+import RPi.GPIO as GPIO 
+from time import sleep 
+GPIO.setwarnings(False) # Everything breaks when this is removed
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
-for x in range(10):
+GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) # Pin 8 is an output
+for x in range(10):   #loop runs 10 times
  GPIO.output(8, GPIO.HIGH) # Turn on
  sleep(1) # Sleep for 1 second
  GPIO.output(8, GPIO.LOW) # Turn off
