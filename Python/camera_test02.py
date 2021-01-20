b7ac60ac23c1f2f56be56ef7f2694631b00ae29e
vwellmo57 <vwellmo57@charlottevilleschools.org>
2021-01-20 18:13:49 +0000

    added color swap

--- Python/camera_test02.py
+++ Python/camera_test02.py
@@ -6,6 +6,7 @@ print("running!")
 with picamera.PiCamera() as camera:
     camera.resolution = (1024, 768)
     camera.start_preview()
+    camera.image_effect = 'colorswap'
     # Camera warm-up time
     time.sleep(2)
     camera.capture('test2.jpg')
