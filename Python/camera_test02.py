ee26364feaeca5ebd73ca83c1882e981b04195bc
vwellmo57 <55098291+vwellmo57@users.noreply.github.com>
2021-01-22 13:08:56 -0500

    Update camera_test02.py

--- Python/camera_test02.py
+++ Python/camera_test02.py
@@ -29,10 +29,34 @@ print("running pic 3")
 
 with picamera.PiCamera() as camera:
     camera.resolution = (1024, 768)
-    camera.image_effect = 'blur'
+    camera.image_effect = 'gpen'
     camera.start_preview()
     # Camera warm-up time
     time.sleep(2)
     camera.capture('test3.jpg')
 
 print("done with pic 3")
+
+print("running pic 4")
+
+with picamera.PiCamera() as camera:
+    camera.resolution = (1024, 768)
+    camera.image_effect = 'solarize'
+    camera.start_preview()
+    # Camera warm-up time
+    time.sleep(2)
+    camera.capture('test4.jpg')
+
+print("done with pic 4")
+
+print("running pic 5")
+
+with picamera.PiCamera() as camera:
+    camera.resolution = (1024, 768)
+    camera.image_effect = 'cartoon'
+    camera.start_preview()
+    # Camera warm-up time
+    time.sleep(2)
+    camera.capture('test5.jpg')
+
+print("done with pic 5")
