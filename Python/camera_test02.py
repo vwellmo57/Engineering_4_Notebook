0c67b7cb1d9973ef629dcbbb43cd9aa18cd5dd27
vwellmo57 <55098291+vwellmo57@users.noreply.github.com>
2021-01-22 12:39:21 -0500

    Update camera_test02.py

--- Python/camera_test02.py
+++ Python/camera_test02.py
@@ -1,7 +1,7 @@
 import time
 import picamera
 
-print("running!")
+print("running! 1")
 
 with picamera.PiCamera() as camera:
     camera.resolution = (1024, 768)
@@ -11,4 +11,16 @@ with picamera.PiCamera() as camera:
     time.sleep(2)
     camera.capture('test2.jpg')
 
-print("done!")
+print("done! 1")
+
+print("running 2!")
+
+with picamera.PiCamera() as camera:
+    camera.resolution = (1024, 768)
+    camera.start_preview()
+    camera.image_effect = 'none'
+    # Camera warm-up time
+    time.sleep(2)
+    camera.capture('test3.jpg')
+
+print("done! 2")
