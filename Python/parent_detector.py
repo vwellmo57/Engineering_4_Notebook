1d7d4f16124f475c63d54a235432b5a84d26f564
vwellmo57 <55098291+vwellmo57@users.noreply.github.com>
2021-01-25 12:44:08 -0500

    Update parent_detector.py

--- Python/parent_detector.py
+++ Python/parent_detector.py
@@ -8,6 +8,6 @@ camera = PiCamera()
 while True:
     filename = "{0:%Y}-{0:%m}-{0:%d}".format(now)
     pir.wait_for_motion()
-    camera.start_recording(filename)
+    camera.start_recording(parent.jpg)
     pir.wait_for_no_motion()
     camera.stop_recording()
