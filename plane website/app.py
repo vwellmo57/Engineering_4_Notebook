1824f3d10976a7137a3fa2664defb5e896101058
vwellmo57 <55098291+vwellmo57@users.noreply.github.com>
2021-03-22 17:52:49 -0400

    fixed philip

--- plane website/app.py
+++ plane website/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 import RPi.GPIO as GPIO
 from time import sleep
-
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.OUT)
 pwm=GPIO.PWM(11, 50)
@@ -22,13 +22,13 @@ app = Flask(name)
 @app.route("/", methods=["GET","POST"])
 def index():
     print(request.method)
-        if request.method == 'POST':
-            if request.form.get('button1') == 'button1':
-                SetAngle(0)
-                sleep(1) 
-                SetAngle(90)
-                sleep(1) 
-                SetAngle(0) 
+    if request.method == 'POST':
+        if request.form.get('button1') == 'button1':
+            SetAngle(0)
+            sleep(1) 
+            SetAngle(90)
+            sleep(1) 
+            SetAngle(0) 
 
 pwm.stop()
 GPIO.cleanup()
