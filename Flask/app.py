c43125c615a59db14ca7639a3259961d759b52a8
vwellmo57 <55098291+vwellmo57@users.noreply.github.com>
2021-03-22 18:35:47 -0400

    Update app.py

--- Flask/app.py
+++ Flask/app.py
@@ -1,40 +1,43 @@
 from flask import Flask, render_template, request
 import RPi.GPIO as GPIO
-from time import sleep
+
 GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.OUT)
-pwm=GPIO.PWM(11, 50)
-pwm.start(0)
-
-def SetAngle(angle):
-    duty = angle / 18 + 2
-    GPIO.output(11, True)
-    pwm.ChangeDutyCycle(duty)
-    sleep(1)
-    GPIO.output(11, False)
-    pwm.ChangeDutyCycle(0)
-
-
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17,GPIO.OUT)
+GPIO.setup(12,GPIO.OUT)
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET","POST"])
 def index():
-    print(request.method)
-    if request.method == 'POST':
-        if request.form.get('button1') == 'button1':
-            SetAngle(0)
-            sleep(1) 
-            SetAngle(90)
-            sleep(1) 
-            SetAngle(0)
-	else:
-            # pass # unknown
-            return render_template("index.html") 
+	print(request.method)
+        if request.method == 'POST':
+            if request.form.get('button1') == 'button1':
+               
+               	
+		GPIO.output(17,GPIO.HIGH)
 
-pwm.stop()
-GPIO.cleanup()
+            elif  request.form.get('button2') == 'button2':
+               
+		GPIO.output(17,GPIO.LOW)
+
+            elif  request.form.get('button3') == 'button3':
+               
+		GPIO.output(12,GPIO.HIGH)
+
+            elif  request.form.get('button4') == 'button4':
+                
+		GPIO.output(12,GPIO.LOW)
+
+            else:
+                
+                return render_template("index.html")
+
+        elif request.method == 'GET':
+           
+            print("No Post Back Call")
+        return render_template("index.html")
+
 
 if __name__ == "__main__":
      app.run(host="0.0.0.0", port=80)
