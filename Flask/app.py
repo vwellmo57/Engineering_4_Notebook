1805ce964a2e97237be4438ea41442573efb6b83
vwellmo57 <55098291+vwellmo57@users.noreply.github.com>
2021-04-01 20:46:40 -0400

    Update app.py

--- Flask/app.py
+++ Flask/app.py
@@ -10,7 +10,6 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET","POST"])
 def index():
-    def index():
     print(request.method)
 if __name__ == "__main__":
      app.run(host="0.0.0.0", port=80)
